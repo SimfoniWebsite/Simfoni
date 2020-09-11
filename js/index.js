81fc631f5a6c96487bccf9b49973a3e1fad97bf2
j-lindsey <j-lindsey@users.noreply.github.com>
2020-09-11 15:21:45 -0600

    fixed icon issues

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function toggleMenu() {
     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
   } else {
     menu.classList.add("active");
-    /*toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";*/
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
   }
 }
 /* Activate Submenu */
