cdaed1580c28d4140b9a35daebf9da6ef193000a
j-lindsey <j-lindsey@users.noreply.github.com>
2020-09-15 16:58:53 -0600

    created form data object

--- js/development.js
+++ js/development.js
@@ -21,3 +21,25 @@ function toggleOrganization() {
 document.querySelectorAll('input[name="indOrOrg"]').forEach(item => {
     item.addEventListener('change', toggleOrganization)
 });
+
+/*submit information on form to database/Simfoni*/
+document.querySelector('.submitDev').addEventListener('click', submitDev);
+
+
+function submitDev() {
+    /*collect info into javascript object*/
+    let devSubmission = document.getElementById('devForm');
+    let devData = new FormData(devSubmission);
+    let devObj = {};
+    /*converts formdata to javascript object*/
+    devData.forEach((value, key) => { devObj[key] = value });
+    /*create array of checked submission needs and add to object*/
+    let subNeeds = document.getElementsByName('subNeeds');
+    let subNeedArr = [];
+    for (let item of subNeeds) {
+        if (item.checked) {   
+            subNeedArr.push(item.id);
+        }
+    }
+    devObj.subNeeds = subNeedArr;
+}
