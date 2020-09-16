92dfcfaaf9d5cb2b62b51a23b7ed4be605f6f956
j-lindsey <j-lindsey@users.noreply.github.com>
2020-09-16 16:59:33 -0600

    basic site layout complete

--- js/busOpportunity.js
+++ js/busOpportunity.js
@@ -1,3 +1,13 @@
+/*create header*/
+fetch('https://jsonplaceholder.typicode.com/albums/1')
+    .then(response => response.json())
+    .then(item => {
+        let h1 = document.createElement('h1');
+        let text = document.createTextNode(item.title);
+        h1.appendChild(text);
+        document.querySelector('header').appendChild(h1);
+    })
+
 /*fetching fake action items and populating to website*/
 fetch('https://jsonplaceholder.typicode.com/todos?userId=1')
     .then(response => response.json())
@@ -18,11 +28,26 @@ fetch('https://jsonplaceholder.typicode.com/comments/1')
         let text = document.createTextNode(detail.body);
         p.appendChild(text);
         document.querySelector('.details').appendChild(p);
-
     })
 
 
-    /*Create embeded video on page*/
+/*Create embeded video on page will be changed later*/
+function video(videolink) {
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('width', '560');
+    iframe.setAttribute('height', '315');
+    iframe.setAttribute('src', videolink);
+    iframe.setAttribute('frameborder', '0');
+    iframe.setAttribute('allow', 'accelerometer');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('allow', 'clipboard-write');
+    iframe.setAttribute('allow', 'encrypted-media');
+    iframe.setAttribute('allow', 'gyroscope');
+    iframe.setAttribute('allow', 'picture-in-picture');
+    iframe.setAttribute('allowfullscreen', 'true');
+    document.querySelector('.video').appendChild(iframe);
+}
 
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/5polY4u0sRU" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+video("https://www.youtube.com/embed/5polY4u0sRU");
+
 
