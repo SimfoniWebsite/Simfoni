eff37ed11692ca9de252e8ffcb428c5540b24f0b
tariq <tariqbaiggeo@gmail.com>
2020-10-07 14:36:19 -0600

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const app = express();
-//==
-//app.use(express.static(__dirname + './login'));
-//===
 
 app.use(express.json());
-//app.use('/register', require('./register'));
+app.use('/register', require('./register'));
 app.use('/venture', require('./venture'));
+app.use('/internal', require('./internal'));
 
-// app.use('/internal', require('./internal'));
-// app.use('/entrepreneur', require('./entrepreneur'));
-app.use('/login', require('./login'));
 module.exports = app;
